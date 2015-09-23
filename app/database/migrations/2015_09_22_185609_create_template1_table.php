bc8c5dbc2c9d53437988e5b2ea85b81c2439fa8f
Dylan Perenchio <perenchiod@gmail.com>
2015-09-23 09:17:32 -0500

    Pushing my load

--- app/database/migrations/2015_09_22_185609_create_template1_table.php
+++ app/database/migrations/2015_09_22_185609_create_template1_table.php
@@ -15,6 +15,39 @@ class CreateTemplate1Table extends Migration {
 		Schema::create('template1', function(Blueprint $table)
 		{
 			$table->increments('id');
+			$table->string('navbarTitle');
+
+			$table->string('headerJobTitle');
+			$table->string('headerDescription');
+
+			$table->string('workExperienceYear');
+			$table->string('workExperienceTitle');
+			$table->string('workExperienceDescription');
+			$table->string('workExperienceExtraText');
+
+			$table->decimal('skillPercent', 2);
+			$table->string('skillTitle');
+
+			$table->string('portfolioPicture');
+			$table->string('portfolioDescription');
+
+			$table->string('contactDesciption');
+			$table->string('contactLinkedin')->nullable();
+			$table->string('contactFacebook')->nullable();
+			$table->string('contactTwitter')->nullable();
+			
+			//Colors
+			$table->string('navbarColor');
+
+			$table->string('headerNameColor');
+			$table->string('headerDescriptionColor');
+			$table->string('headerBackgroundImage');
+
+			$table->string('portfolioImage');
+			$table->string('portfolioDescriptionColor');
+
+			$table->string('contactDesciptionColor');
+			
 			$table->timestamps();
 		});
 	}
