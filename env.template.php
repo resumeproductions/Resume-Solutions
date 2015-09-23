454c78b86f4e54a588a3d69baed654502c80cb71
Dylan Perenchio <perenchiod@gmail.com>
2015-09-23 13:11:17 -0500

    User login

--- env.template.php
+++ env.template.php
@@ -5,6 +5,15 @@
 	    'DB_NAME' => '',
 	    'DB_USER' => '',
 	    'DB_PASS' => '', 
+
+	    'USER_FIRST_NAME' => '',
+		'USER_LAST_NAME' => '',
+		'USER_EMAIL' => '',
+		'USER_ADDRESS' => '',
+		'USER_ZIP' => '',
+		'USER_STATE' => '',
+		'USER_PHONE_NUMBER' => '',
+		'USER_PASSWORD' => ''
 	);
 
 ?>
