e6abe613839d2ccc0e882be8e1ae94fce984f009
David Simonelli <david8simonelli@gmail.com>
2015-09-29 10:46:52 -0500

    baby bear

--- app/views/templates/DPS_create_template.blade.php
+++ app/views/templates/DPS_create_template.blade.php
@@ -12,18 +12,124 @@
 </head>
 <body>
 	<div class='page1 pagesle'>
+		<input placeholder='' class='Inpage1' name='' id='input11'>
+		<input placeholder='' class='Inpage1' name='' id='input12'>
+		<input placeholder='' class='Inpage1' name='' id='input13'>
 	</div>
 	<div class='page2 pagesle'>
+		<input placeholder='' class='Inpage2' name='' id='input21'>
+		<input placeholder='' class='Inpage2' name='' id='input22'>
+		<input placeholder='' class='Inpage2' name='' id='input23'>
 	</div>
 	<div class='page3 pagesle'>
+		<input placeholder='' class='Inpage3' name='' id='input31'>
+		<input placeholder='' class='Inpage3' name='' id='input32'>
+		<input placeholder='' class='Inpage3' name='' id='input33'>
+		<input placeholder='' class='Inpage3' name='' id='input34'>
+		<input placeholder='' class='Inpage3' name='' id='input35'>
+		<input placeholder='' class='Inpage3' name='' id='input36'>
+		<input placeholder='' class='Inpage3' name='' id='input37'>
+		<input placeholder='' class='Inpage3' name='' id='input38'>
+		<input placeholder='' class='Inpage3' name='' id='input39'>
+		<input placeholder='' class='Inpage3' name='' id='input310'>
+		<input placeholder='' class='Inpage3' name='' id='input311'>
+		<input placeholder='' class='Inpage3' name='' id='input312'>
 	</div>
 	<div class='page4 pagesle'>
+		<input placeholder='' class='Inpage4' name='' id='input41'>
+		<input placeholder='' class='Inpage4' name='' id='input42'>
+		<input placeholder='' class='Inpage4' name='' id='input43'>
+		<input placeholder='' class='Inpage4' name='' id='input44'>
+		<input placeholder='' class='Inpage4' name='' id='input45'>
+		<input placeholder='' class='Inpage4' name='' id='input46'>
+		<input placeholder='' class='Inpage4' name='' id='input47'>
+		<input placeholder='' class='Inpage4' name='' id='input48'>
+		<input placeholder='' class='Inpage4' name='' id='input49'>
+		<input placeholder='' class='Inpage4' name='' id='input410'>
+		<input placeholder='' class='Inpage4' name='' id='input411'>
 	</div>
 	<div class='page5 pagesle'>
+		<input placeholder='' class='Inpage5' name='' id='input51'>
+		<input placeholder='' class='Inpage5' name='' id='input52'>
+		<input placeholder='' class='Inpage5' name='' id='input53'>
+		<input placeholder='' class='Inpage5' name='' id='input54'>
+		<input placeholder='' class='Inpage5' name='' id='input55'>
+		<input placeholder='' class='Inpage5' name='' id='input56'>
+		<input placeholder='' class='Inpage5' name='' id='input57'>
+		<input placeholder='' class='Inpage5' name='' id='input58'>
+		<input placeholder='' class='Inpage5' name='' id='input59'>
+		<input placeholder='' class='Inpage5' name='' id='input510'>
+		<input placeholder='' class='Inpage5' name='' id='input511'>
+		<input placeholder='' class='Inpage5' name='' id='input512'>
+		<input placeholder='' class='Inpage5' name='' id='input513'>
+		<input placeholder='' class='Inpage5' name='' id='input514'>
+		<input placeholder='' class='Inpage5' name='' id='input514'>
+		<input placeholder='' class='Inpage5' name='' id='input516'>
+		<input placeholder='' class='Inpage5' name='' id='input517'>
+		<input placeholder='' class='Inpage5' name='' id='input518'>
+		<input placeholder='' class='Inpage5' name='' id='input519'>
+		<input placeholder='' class='Inpage5' name='' id='input520'>
+		<input placeholder='' class='Inpage5' name='' id='input521'>
+		<input placeholder='' class='Inpage5' name='' id='input522'>
 	</div>
 	<div class='page6 pagesle'>
+		<input placeholder='' class='Inpage6' name='' id='input61'>
+		<input placeholder='' class='Inpage6' name='' id='input62'>
+		<input placeholder='' class='Inpage6' name='' id='input63'>
+		<input placeholder='' class='Inpage6' name='' id='input64'>
+		<input placeholder='' class='Inpage6' name='' id='input65'>
+		<input placeholder='' class='Inpage6' name='' id='input66'>
+		<input placeholder='' class='Inpage6' name='' id='input67'>
+		<input placeholder='' class='Inpage6' name='' id='input68'>
+		<input placeholder='' class='Inpage6' name='' id='input69'>
+		<input placeholder='' class='Inpage6' name='' id='input610'>
+		<input placeholder='' class='Inpage6' name='' id='input611'>
+		<input placeholder='' class='Inpage6' name='' id='input612'>
+		<input placeholder='' class='Inpage6' name='' id='input613'>
+		<input placeholder='' class='Inpage6' name='' id='input614'>
+		<input placeholder='' class='Inpage6' name='' id='input614'>
+		<input placeholder='' class='Inpage6' name='' id='input616'>
+		<input placeholder='' class='Inpage6' name='' id='input617'>
+		<input placeholder='' class='Inpage6' name='' id='input618'>
+		<input placeholder='' class='Inpage6' name='' id='input619'>
+		<input placeholder='' class='Inpage6' name='' id='input620'>
+		<input placeholder='' class='Inpage6' name='' id='input621'>
+		<input placeholder='' class='Inpage6' name='' id='input622'>
+		<input placeholder='' class='Inpage6' name='' id='input623'>
+		<input placeholder='' class='Inpage6' name='' id='input624'>
+		<input placeholder='' class='Inpage6' name='' id='input625'>
+		<input placeholder='' class='Inpage6' name='' id='input626'>
+		<input placeholder='' class='Inpage6' name='' id='input627'>
+		<input placeholder='' class='Inpage6' name='' id='input628'>
+		<input placeholder='' class='Inpage6' name='' id='input629'>
+		<input placeholder='' class='Inpage6' name='' id='input630'>
+		<input placeholder='' class='Inpage6' name='' id='input631'>
+		<input placeholder='' class='Inpage6' name='' id='input632'>
+		<input placeholder='' class='Inpage6' name='' id='input633'>
+		<input placeholder='' class='Inpage6' name='' id='input634'>
+		<input placeholder='' class='Inpage6' name='' id='input635'>
+		<input placeholder='' class='Inpage6' name='' id='input636'>
+		<input placeholder='' class='Inpage6' name='' id='input637'>
+		<input placeholder='' class='Inpage6' name='' id='input638'>
+		<input placeholder='' class='Inpage6' name='' id='input639'>
+		<input placeholder='' class='Inpage6' name='' id='input640'>
 	</div>
 	<div class='page7 pagesle'>
+		<input placeholder='' class='Inpage7' name='' id='input71'>
+		<input placeholder='' class='Inpage7' name='' id='input72'>
+		<input placeholder='' class='Inpage7' name='' id='input73'>
+		<input placeholder='' class='Inpage7' name='' id='input74'>
+		<input placeholder='' class='Inpage7' name='' id='input75'>
+		<input placeholder='' class='Inpage7' name='' id='input76'>
+		<input placeholder='' class='Inpage7' name='' id='input77'>
+		<input placeholder='' class='Inpage7' name='' id='input78'>
+		<input placeholder='' class='Inpage7' name='' id='input79'>
+		<input placeholder='' class='Inpage7' name='' id='input710'>
+		<input placeholder='' class='Inpage7' name='' id='input711'>
+		<input placeholder='' class='Inpage7' name='' id='input712'>
+		<input placeholder='' class='Inpage7' name='' id='input713'>
+		<input placeholder='' class='Inpage7' name='' id='input714'>
+		<input placeholder='' class='Inpage7' name='' id='input715'>
 	</div>
 	<button id='right' type="button" class="btn btn-default btn-lg">
 		<i class="fa fa-arrow-right fa-2x">
