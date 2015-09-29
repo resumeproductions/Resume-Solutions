3d0f799a9eaa0f6c8ca2e60f6eef75287da42da1
Dylan Perenchio <perenchiod@gmail.com>
2015-09-29 10:30:08 -0500

    Fixing up our commit errors

--- app/views/templates/create_template.blade.php
+++ app/views/templates/create_template.blade.php
@@ -18,33 +18,33 @@
 	</div>
 	<div class='page2 pagesle'>
 		<textarea placeholder='Description of General Previous Work' class='Inpage2' name='workExperienceExtraText'	id='input21'></textarea>
-		<input 	  placeholder='Year of Work' class='Inpage2' name='workExperienceYear' 		id='input22'>
-		<input 	  placeholder='Work Title' class='Inpage2' name='workExperienceTitle' 		id='input23'>
-		<textarea placeholder='Work Description' class='Inpage2' name='workExperienceDescription'  id='input24'></textarea>
-		<input 	  placeholder='Year of Work' class='Inpage2' name='workExperienceYear' 		id='input25'>
-		<input 	  placeholder='Work Title' class='Inpage2' name='workExperienceTitle' 		id='input26'>
-		<textarea placeholder='Work Description' class='Inpage2' name='workExperienceDescription'  id='input27'></textarea>
-		<input 	  placeholder='Year of Work' class='Inpage2' name='workExperienceYear' 		id='input28'>
-		<input 	  placeholder='Work Title' class='Inpage2' name='workExperienceTitle' 		id='input29'>
-		<textarea placeholder='Work Description' class='Inpage2' name='workExperienceDescription'  id='input210'></textarea>
+		<input 	  placeholder='Year of Work' class='Inpage2' name='workExperienceYear[]' 		id='input22'>
+		<input 	  placeholder='Work Title' class='Inpage2' name='workExperienceTitle[]' 		id='input23'>
+		<textarea placeholder='Work Description' class='Inpage2' name='workExperienceDescription[]'  id='input24'></textarea>
+		<input 	  placeholder='Year of Work' class='Inpage2' name='workExperienceYear[]' 		id='input25'>
+		<input 	  placeholder='Work Title' class='Inpage2' name='workExperienceTitle[]' 		id='input26'>
+		<textarea placeholder='Work Description' class='Inpage2' name='workExperienceDescription[]'  id='input27'></textarea>
+		<input 	  placeholder='Year of Work' class='Inpage2' name='workExperienceYear[]' 		id='input28'>
+		<input 	  placeholder='Work Title' class='Inpage2' name='workExperienceTitle[]' 		id='input29'>
+		<textarea placeholder='Work Description' class='Inpage2' name='workExperienceDescription[]'  id='input210'></textarea>
 	</div>
 	<div class='page3 pagesle'>
-		<input class='Inpage3' name='skillPercent' 	id='input31'>
-		<input class='Inpage3' placeholder='Skill Title' name='skillTitle' 	id='input32'>
-		<input class='Inpage3' name='skillPercent' 	id='input33'>
-		<input class='Inpage3' placeholder='Skill Title' name='skillTitle' 	id='input34'>
-		<input class='Inpage3' name='skillPercent' 	id='input35'>
-		<input class='Inpage3' placeholder='Skill Title' name='skillTitle' 	id='input36'>
-		<input class='Inpage3' name='skillPercent' 	id='input37'>
-		<input class='Inpage3' placeholder='Skill Title' name='skillTitle' 	id='input38'>
+		<input class='Inpage3' name='skillPercent[]' 	id='input31'>
+		<input class='Inpage3' placeholder='Skill Title' name='skillTitle[]' 	id='input32'>
+		<input class='Inpage3' name='skillPercent[]' 	id='input33'>
+		<input class='Inpage3' placeholder='Skill Title' name='skillTitle[]' 	id='input34'>
+		<input class='Inpage3' name='skillPercent[]' 	id='input35'>
+		<input class='Inpage3' placeholder='Skill Title' name='skillTitle[]' 	id='input36'>
+		<input class='Inpage3' name='skillPercent[]' 	id='input37'>
+		<input class='Inpage3' placeholder='Skill Title' name='skillTitle[]' 	id='input38'>
 	</div>
 	<div class='page4 pagesle'>
-		<input class='Inpage4'  placeholder='Portfolio Title' name='portfolioDescription'  id='input41'>
-		<button class='Inpage4' name='portfolioPicture' 		id='input42'></button>
-		<input class='Inpage4'  placeholder='Portfolio Title' name='portfolioDescription' 	id='input43'>
-		<button class='Inpage4' name='portfolioPicture' 		id='input44'></button>
-		<input class='Inpage4'  placeholder='Portfolio Title' name='portfolioDescription' 	id='input45'>
-		<button class='Inpage4' name='portfolioPicture' 		id='input46'></button>
+		<input class='Inpage4'  placeholder='Portfolio Title' name='portfolioDescription[]'  id='input41'>
+		<button class='Inpage4' name='portfolioPicture[]' 		id='input42'></button>
+		<input class='Inpage4'  placeholder='Portfolio Title' name='portfolioDescription[]' 	id='input43'>
+		<button class='Inpage4' name='portfolioPicture[]' 		id='input44'></button>
+		<input class='Inpage4'  placeholder='Portfolio Title' name='portfolioDescription[]' 	id='input45'>
+		<button class='Inpage4' name='portfolioPicture[]' 		id='input46'></button>
 	</div>
 	<div class='page5 pagesle'>
 		<input class='Inpage5' placeholder=' Facebook Link' name='contactFacebook' id='input51'>
@@ -52,15 +52,16 @@
 		<input class='Inpage5' placeholder=' Twitter Link' name='contactTwitter' id='input53'>
 		<input class='Inpage5' placeholder=' Contact Description' name='contactDescription' id='input54'>
 	</div>
+	<button type="submit" class='Inpage5' id='submitTemplate1'>Submit</button>
 	<button id='right' type="button" class="btn btn-default btn-lg">
 		<i class="fa fa-arrow-right fa-2x">
-		</button>
-		<button id='left' type="button" class="btn btn-default btn-lg">
-			<i class="fa fa-arrow-left fa-2x">
-			</button>
-			@stop
+	</button>
+	<button id='left' type="button" class="btn btn-default btn-lg">
+		<i class="fa fa-arrow-left fa-2x">
+	</button>
+		@stop
 
-			@section('script')
+		@section('script')
 			<script type="text/javascript">
 			$(document).ready(function() {
 				var findRigh = document.getElementById('right');
